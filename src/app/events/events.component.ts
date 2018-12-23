27e0e43f08b44c02ed034b0e868f0e5f1608518b
SarthakSri98 <sarthaksri1998@gmail.com>
2018-12-24 02:18:01 +0530

    all events added

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -16,20 +16,23 @@ export class EventsComponent implements OnInit {
     this.eventList=[
       { img:'https://preview.ibb.co/gtWuTA/e-cell-poster.jpg',eventName:'E-Cell Freshmen Orientation', eventDescription:'Description' },
       { img:'https://preview.ibb.co/f29yoA/ecell-poster4-1.png',eventName:'E-Cell Case study competition', eventDescription:'Description' },
-      { img:'https://preview.ibb.co/f29yoA/ecell-poster4-1.png',eventName:'E-Cell Case study competition', eventDescription:'Description' },
-      { img:'https://preview.ibb.co/f29yoA/ecell-poster4-1.png',eventName:'E-Cell Case study competition', eventDescription:'Description' },
-      { img:'https://preview.ibb.co/f29yoA/ecell-poster4-1.png',eventName:'E-Cell Case study competition', eventDescription:'Description' },
-      { img:'https://preview.ibb.co/f29yoA/ecell-poster4-1.png',eventName:'E-Cell Case study competition', eventDescription:'Description' },
-      { img:'https://preview.ibb.co/f29yoA/ecell-poster4-1.png',eventName:'E-Cell Case study competition', eventDescription:'Description' },
-      { img:'https://preview.ibb.co/f29yoA/ecell-poster4-1.png',eventName:'E-Cell Case study competition', eventDescription:'Description' },
-      { img:'https://preview.ibb.co/f29yoA/ecell-poster4-1.png',eventName:'E-Cell Case study competition', eventDescription:'Description' },
-      { img:'https://preview.ibb.co/f29yoA/ecell-poster4-1.png',eventName:'E-Cell Case study competition', eventDescription:'Description' },
-
+      { img:'https://i.ibb.co/StPj2rN/movie-screening.png',eventName:'E-Cell Movie Screening', eventDescription:'Description' },
+      { img:'https://i.ibb.co/kJZv0dt/gd.png',eventName:'E-Cell GD invitation', eventDescription:'Description' },
+      { img:'https://i.ibb.co/Xt2dVkJ/is-college-the-right-time.png',eventName:'Session on "Is college the right time"', eventDescription:'Description' },
+      { img:'https://i.ibb.co/VMP4XbM/business-plan.png',eventName:'Session on "How to create a business model" ', eventDescription:'Description' },
+      { img:'https://i.ibb.co/Yhjk1vv/life-of-an-entrepreneus.jpg',eventName:'Session "Life of an entrepreneur" ', eventDescription:'Description' },
+      { img:'https://i.ibb.co/0XB86rt/how-to-pitch-an-idea.jpg',eventName:'Session on "Pitching an idea" ', eventDescription:'Description' },
+      { img:'https://i.ibb.co/zGQsJYG/debate-comp.png',eventName:'E-Cell Debate competition', eventDescription:'Description' },
+      { img:'https://i.ibb.co/Kb9yTGX/d2c-hackathon.jpg',eventName:'E-Cell Dare2compete hackathon', eventDescription:'Description' },
+      { img:'https://i.ibb.co/k14YB4R/quiz.jpg',eventName:'E-Cell Quiz Competition', eventDescription:'Description' },
+      { img:'https://i.ibb.co/99yrfg1/d2c-quiz.png',eventName:'E-Cell Dare2compete Quiz', eventDescription:'Description' },
+      { img:'https://i.ibb.co/Fw3dtwm/startup-survey.png',eventName:'E-Cell Statup Survey', eventDescription:'Description' },
+      
 
     ]
 
   }
-  height='auto';
+ height='auto';
   width='400px';  
   events: string[]=['Case study event going on','Conducted the freshment orientation'];
 
